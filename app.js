49d89d1f72442a7f00b32679cb51a3264f8ae476
Hannyajin <talmo.christian@gmail.com>
2014-03-31 09:44:06 +0300

    added monogdb

--- app.js
+++ app.js
@@ -4,12 +4,30 @@
  */
 
 var express = require('express');
-//var routes = require('./routes');
-//var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = module.exports = express();
+
+// connect to remote database
+var dataBaseUrl = "mongodb://yoshi:kamula20140o9i8uy7u8i9@ds051077.mongolab.com:51077/misc";
+var collections = ["kamula"];
+
+mongodb = require("mongojs").connect(dataBaseUrl, collections);
+
+/*
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/misc', function(err, db) {
+	if (err) throw err;
+
+	var collection = db.collection('kamula');
+
+	collection.find().
+
+});
+*/
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -29,15 +47,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
   app.use(express.logger('dev'));
 }
-
-//app.get('/', routes.index);
-//app.get('/users', user.list);
-
-//app.get('/', function(req, res, next) {
-	//res.send({text: 'heres some text' });
-	//res.render('index', {title: 'Title of App'});
-//});
-
 
 require('./routes');
 
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@
  * GET home page.
  */
 
-//exports.index = function(req, res){
-//  res.render('index', { title: 'Express' });
-//};
-
 app = require('../app');
 
 app.get('/', function(req, res, next) {
 	res.render('index', {title: 'Awsome Title'});
+});
+
+
+app.get('/find', function(req, res) {
+	mongodb.kamula.find({}, function(err, data) {
+		res.send(data);
+	});
 });
