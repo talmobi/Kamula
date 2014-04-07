aa591869aa4e57da2551b1b44da2d6b0e54a94e7
Hannyajin <talmo.christian@gmail.com>
2014-04-07 12:18:35 +0300

    new files (refactored code from old api.js)

---
+++ routes/api.js
@@ -0,0 +1,73 @@
+module.exports = function(app) {
+	//Lisää käyttäjän järjestelmään	Runkona lisättävä käyttäjä JSON-muodossa
+	app.post('/api/users', function(req, res) {
+		register(req,res);
+	});
+
+	// Hakee käyttäjän tiedot. Palauttaa käyttäjän tiedot JSON-muodossa
+	app.get('/api/users/:user', function(req, res) {
+		var user = req.params.user;
+
+		mongoose.model('User').findOne({ lowercaseName: user.toLowerCase() }, function(err, doc) {
+			if (err) throw err;
+
+			if (doc) { // exists
+				var data = {
+					user: doc.user,
+					name: doc.name,
+					email: doc.email
+				}
+				res.send( data );
+			} else {	// doesn't exists
+				res.send( 404, { message: "That user doesn't exist.", errorSource: "" } );
+			}
+		});
+	});
+
+
+	// Päivittää käyttäjän tietoja. Runkona käyttäjän uudet tiedot JSON-muodossa,
+	// vaatii tunnistautumisen
+	app.put('/api/users/:user', function(req, res) {
+		var json = req.body;
+
+		mongoose.model('User').findOne({ lowercaseName: json.user.toLowerCase() }, function(err, doc) {
+			if (err) {
+				console.log(err);
+				res.send(400);
+			}
+
+			if (doc) { // exists
+				var newdata = {
+					name: json.name,
+					email: json.email,
+					password: json.password
+				}
+
+				// update the users info
+				mongoose.model('User').update( doc, newdata );
+
+				res.send(200); // OK! Bojangles!
+			} else {
+				res.send(404); // user doesn't exist
+			}
+		});
+	});
+
+	// Poistaa annetun käyttäjän. Vaatii tunnistautumisen.
+	app.delete('/api/users/:user', function(req, res) {
+		var json = req.body;
+
+		mongoose.model('User').findOne({ lowercaseName: json.user.toLowerCase() }, function(err, doc) {
+			if (err) {
+				console.log(err);
+			}
+
+			if (doc) {
+					mongoose.model('User').remove( { lowercaseName: json.user.toLowerCase() } );
+			} else {
+				// ei ole olemassa
+				res.send(404);
+			}
+		});
+	});
+}
